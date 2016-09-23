41ceaac0bbd84a05e288781446a2d282af428936
Christian Flothmann <christian.flothmann@xabbuh.de>
2016-09-23 20:36:34 +0200

    add missing file header

--- spec/Interaction/InteractionDefinitionSpec.php
+++ spec/Interaction/InteractionDefinitionSpec.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the xAPI package.
+ *
+ * (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace spec\Xabbuh\XApi\Model\Interaction;
 
 use PhpSpec\ObjectBehavior;
