88770e076c13eca9200bd7828a5998b37b8627e9
Christian Flothmann <christian.flothmann@sensiolabs.de>
2017-03-27 10:48:30 +0200

    [#55] minor rewording of deprecation message

--- src/Statement.php
+++ src/Statement.php
@@ -156,7 +156,7 @@ final class Statement
      */
     public function withTimestamp(\DateTime $timestamp = null)
     {
-        @trigger_error(sprintf('The "%s()" method is deprecated since 1.2 and will be removed in 3.0. Use "%s::withCreated()" method instead.', __METHOD__, __CLASS__), E_USER_DEPRECATED);
+        @trigger_error(sprintf('The "%s()" method is deprecated since 1.2 and will be removed in 3.0. Use "%s::withCreated()" instead.', __METHOD__, __CLASS__), E_USER_DEPRECATED);
 
         $statement = clone $this;
         $statement->created = $timestamp;
@@ -284,7 +284,7 @@ final class Statement
      */
     public function getTimestamp()
     {
-        @trigger_error(sprintf('The "%s()" method is deprecated since 1.2 and will be removed in 3.0. Use "%s::getCreated()" method instead.', __METHOD__, __CLASS__), E_USER_DEPRECATED);
+        @trigger_error(sprintf('The "%s()" method is deprecated since 1.2 and will be removed in 3.0. Use "%s::getCreated()" instead.', __METHOD__, __CLASS__), E_USER_DEPRECATED);
 
         return $this->created;
     }
