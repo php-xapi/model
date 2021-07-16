96390914a6bb9615bfd7a2cffd695ac22f8952fb
Wesley Klop <w.klop@inthere.nl>
2021-07-16 14:44:37 +0200

    Minor code style fixes

--- ecs.php
+++ ecs.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
@@ -12,6 +11,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
         __DIR__ . '/spec',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/rector.php',
     ]);
 
     // run and fix, one by one
--- rector.php
+++ rector.php
@@ -9,15 +9,16 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
-
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
         __DIR__ . '/spec',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/rector.php',
     ]);
+
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_72);
 
     $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::PHP_72);
     $containerConfigurator->import(SetList::CODE_QUALITY);
-
+    $containerConfigurator->import(SetList::PHP_72);
 };
